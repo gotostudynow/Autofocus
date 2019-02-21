63dc7c856bbe7955fbe5e765ccadcec67d8b047b
gotostudynow <1771394237@qq.com>
2019-02-21 11:13:54 +0800

    comment2

--- detect_blur.py
+++ detect_blur.py
@@ -14,7 +14,8 @@ def variance_of_laplacian(image):
 # construct the argument parse and parse the arguments
 
 
-
+//
+//comment 2
 # loop over the input images
 for imagePath in paths.list_images(args["images"]):
 	# load the image, convert it to grayscale, and compute the
